2b7e249ab1c31e87058267cd79a6b20ac87d6a39
William Hleucka <william.hleucka@gmail.com>
2025-09-10 16:27:12 -0600

    wip: perlin noise

--- src/chunk.cpp
+++ src/chunk.cpp
@@ -1,29 +1,32 @@
 #include "chunk.hpp"
 #include "block_type.hpp"
+#include "perlin_noise.hpp"
 #include <glm/glm.hpp>
 
 Chunk::Chunk(const Texture &atlas) {
   m_mesh.textures.push_back(atlas);
 
+  const std::uint32_t seed = 123456u;
+  const siv::PerlinNoise perlin{seed};
+
   for (int x = 0; x < CHUNK_WIDTH; x++) {
     for (int z = 0; z < CHUNK_LENGTH; z++) {
-      for (int y = 0; y < CHUNK_HEIGHT; y++) {
+      const double noise = perlin.octaveNoise0_1(x * 0.01, z * 0.01, 4);
+      const int height = static_cast<int>(noise * 64) + 64;
+
+      for (int y = 0; y < height; y++) {
         BlockType type = BlockType::AIR;
-        if (y < 64) {
-          type = BlockType::STONE;
-        } else if (y < 128) {
-          type = BlockType::DIRT;
-        } else if (y == 128) {
+        if (y == height - 1) {
           type = BlockType::GRASS;
+        } else if (y > height - 5) {
+          type = BlockType::DIRT;
+        } else {
+          type = BlockType::STONE;
         }
 
         if (type != BlockType::AIR) {
           Block block(type, glm::vec3(x, y, z));
 
-          // This is a very inefficient way to merge meshes.
-          // We are just inserting all vertices and indices from each block into
-          // the chunk mesh.
-          // We will need to optimize this later.
           unsigned int offset = m_mesh.vertices.size();
           m_mesh.vertices.insert(m_mesh.vertices.end(),
                                  block.mesh.vertices.begin(),
